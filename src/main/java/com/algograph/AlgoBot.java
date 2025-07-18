1b03aa2de8e94bde7449e27a7ac7f8297c27240e
Kimathi Sedegah <sedkim370@gmail.com>
2025-07-18 11:09:16 +0000

    Update AlgoBot.java

--- src/main/java/com/algograph/AlgoBot.java
+++ src/main/java/com/algograph/AlgoBot.java
@@ -1,11 +1,19 @@
-package com.algobot;
+package com.algograph;
 
+import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class AlgoBot {
-    public static void main(String[] args) throws Exception {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(new AlgoHandler());
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new AlgoHandler());
+            System.out.println("AlgoGraphBot is running...");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
